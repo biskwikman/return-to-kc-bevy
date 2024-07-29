18e20f3441429709c514579622d5163c4b5a204a
Dan Henri <djosephhenri@gmail.com>
2024-07-29 14:23:54 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use bevy::{
+    color::palettes::css::*,
+    prelude::*,
+    reflect::DynamicTypePath,
+    sprite::Anchor,
+    text::{BreakLineOn, Text2dBounds},
+    window::WindowResolution,
+};
+
+fn main() {
+    let window_resolution = WindowResolution::new(800.0, 600.0);
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: window_resolution,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_translation)
+        .run();
+}
+
+#[derive(Component)]
+struct AnimateTranslation;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut query: Query<&Window>) {
+    let window = query.single();
+    let font = asset_server.load("fonts/Mx437_IBM_BIOS.ttf");
+    let font_size = 10.0;
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size,
+        ..default()
+    };
+
+    let text_justification = JustifyText::Center;
+
+    commands.spawn(Camera2dBundle::default());
+
+    for mut y in (-295..(window.resolution.height() / 2.0) as i32).step_by(10) {
+        commands.spawn(Text2dBundle {
+            text: Text::from_section('@', text_style.clone()).with_justify(text_justification),
+            transform: Transform::from_xyz(0.0, y as f32, 1.0),
+            ..default()
+        });
+    }
+}
+
+fn animate_translation(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, (With<Text>, With<AnimateTranslation>)>,
+) {
+    // for mut transform in &mut query {
+    //     transform.translation.x = 100.0 * time.elapsed_seconds().sin() - 400.0;
+    //     transform.translation.y = 100.0 * time.elapsed_seconds().cos();
+    // }
+}
