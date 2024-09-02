dd1f889d5d68c0d3143f5b1501c47ba5a215479e
biskwikman <djosephhenri@gmail.com>
2024-09-03 00:00:35 +0900

    rooms

--- src/map.rs
+++ src/map.rs
@@ -8,7 +8,7 @@ pub fn get_tile_idx(idx_xy: (usize, usize)) -> usize {
 }
 
 pub fn create_map(
-    mut query: Query<(Entity, &mut Tile, &Transform)>,
+    mut set: ParamSet<(Query<(Entity, &mut Tile, &Transform)>, Query<&mut Tile>)>,
     mut map: ResMut<Map>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -20,8 +20,22 @@ pub fn create_map(
         font_size,
         ..default()
     };
-    for (ent, _tile, _transform) in query.iter() {
+    for (ent, mut tile, _transform) in set.p0().iter_mut() {
+        tile.tiletype = TileType::Wall;
         map.tiles.push(ent);
+    }
+
+    let room1 = Rect::new(9., 9., 35., 25.);
+
+    // apply_room_to_map(&room1, map, set.p1());
+
+    for y in room1.min.y as usize + 1..=room1.max.y as usize {
+        for x in room1.min.x as usize + 1..=room1.max.x as usize {
+            set.p1()
+                .get_mut(map.tiles[get_tile_idx((x, y))])
+                .unwrap()
+                .tiletype = TileType::Floor;
+        }
     }
 
     // query
@@ -30,7 +44,7 @@ pub fn create_map(
     //     .1
     //     .tiletype = TileType::Wall;
 
-    for (ent, tile, transform) in query.iter() {
+    for (ent, tile, transform) in set.p0().iter() {
         match tile.tiletype {
             TileType::Wall => {
                 commands.entity(ent).insert(Text2dBundle {
@@ -49,4 +63,13 @@ pub fn create_map(
     }
 }
 
-pub fn apply_room_to_map(room: &Rect, mut map: ResMut<Map>, mut query: Query<&mut Tile>) {}
+pub fn apply_room_to_map(room: &Rect, map: ResMut<Map>, mut query: Query<&mut Tile>) {
+    for y in room.min.y as usize + 1..=room.max.y as usize {
+        for x in room.min.x as usize + 1..=room.max.x as usize {
+            query
+                .get_mut(map.tiles[get_tile_idx((x, y))])
+                .unwrap()
+                .tiletype = TileType::Floor;
+        }
+    }
+}
--- src/rect.rs
+++ src/rect.rs
@@ -6,21 +6,32 @@ use bevy::prelude::*;
 //     pub y2: i32,
 // }
 
-impl Rect {
-    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Rect {
-        Rect {
-            x1: x,
-            y1: y,
-            x2: x + w,
-            y2: y + h,
-        }
+pub trait Intersect {
+    fn is_intersect(&self, other: Rect) -> bool;
+}
+
+impl Intersect for Rect {
+    // pub fn new(x: i32, y: i32, w: i32, h: i32) -> Rect {
+    //     Rect {
+    //         x1: x,
+    //         y1: y,
+    //         x2: x + w,
+    //         y2: y + h,
+    //     }
+    // }
+
+    // fn intersect(&self, other: Rect) -> bool {
+    //     self.x1 <= other.x2 && self.x2 >= other.x1 && self.y1 <= other.y2 && self.x2 >= other.y1
+    // }
+
+    fn is_intersect(&self, other: Rect) -> bool {
+        self.min.x <= other.max.x
+            && self.max.x >= other.min.x
+            && self.min.y <= other.max.y
+            && self.max.y >= other.min.y
     }
 
-    pub fn intersect(&self, other: Rect) -> bool {
-        self.x1 <= other.x2 && self.x2 >= other.x1 && self.y1 <= other.y2 && self.x2 >= other.y1
-    }
-
-    pub fn center(&self) -> (i32, i32) {
-        ((self.x1 + self.x2) / 2, (self.y1 + self.y2) / 2)
-    }
+    // pub fn center(&self) -> (i32, i32) {
+    //     ((self.x1 + self.x2) / 2, (self.y1 + self.y2) / 2)
+    // }
 }
