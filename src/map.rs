63da2e6f75766810824fe1973da323d6f3e4bc34
biskwikman <djosephhenri@gmail.com>
2024-09-06 08:31:32 +0900

    various

--- src/map.rs
+++ src/map.rs
@@ -1,7 +1,7 @@
 use crate::components::*;
-// use crate::rect::*;
 use crate::resources::*;
 use bevy::prelude::*;
+use std::cmp::{max, min};
 
 pub fn get_tile_idx(idx_xy: (usize, usize)) -> usize {
     idx_xy.0 + 80 * idx_xy.1
@@ -25,24 +25,12 @@ pub fn create_map(
         map.tiles.push(ent);
     }
 
-    let room1 = Rect::new(9., 9., 35., 25.);
+    let room1 = Rect::new(0., 0., 35., 25.);
+    let room2 = Rect::new(50., 10., 59., 50.);
 
-    // apply_room_to_map(&room1, map, set.p1());
-
-    for y in room1.min.y as usize + 1..=room1.max.y as usize {
-        for x in room1.min.x as usize + 1..=room1.max.x as usize {
-            set.p1()
-                .get_mut(map.tiles[get_tile_idx((x, y))])
-                .unwrap()
-                .tiletype = TileType::Floor;
-        }
-    }
-
-    // query
-    //     .get_mut(map.tiles[get_tile_idx((12, 12))])
-    //     .unwrap()
-    //     .1
-    //     .tiletype = TileType::Wall;
+    apply_room_to_map(&room1, &map, set.p1());
+    apply_room_to_map(&room2, &map, set.p1());
+    apply_horizontal_tunnel(&map, 35, 50, 20, set.p1());
 
     for (ent, tile, transform) in set.p0().iter() {
         match tile.tiletype {
@@ -63,13 +51,43 @@ pub fn create_map(
     }
 }
 
-pub fn apply_room_to_map(room: &Rect, map: ResMut<Map>, mut query: Query<&mut Tile>) {
-    for y in room.min.y as usize + 1..=room.max.y as usize {
-        for x in room.min.x as usize + 1..=room.max.x as usize {
+pub fn apply_room_to_map(room: &Rect, map: &ResMut<Map>, mut query: Query<&mut Tile>) {
+    for y in room.min.y as usize..=room.max.y as usize {
+        for x in room.min.x as usize..=room.max.x as usize {
             query
                 .get_mut(map.tiles[get_tile_idx((x, y))])
                 .unwrap()
                 .tiletype = TileType::Floor;
+        }
+    }
+}
+
+pub fn apply_horizontal_tunnel(
+    map: &ResMut<Map>,
+    x1: i32,
+    x2: i32,
+    y: i32,
+    mut query: Query<&mut Tile>,
+) {
+    for x in min(x1, x2)..=max(x1, x2) {
+        let idx = get_tile_idx((x as usize, y as usize));
+        if idx > 0 && idx < 80 * 50 {
+            query.get_mut(map.tiles[idx]).unwrap().tiletype = TileType::Floor;
+        }
+    }
+}
+
+pub fn apply_vertical_tunnel(
+    map: &ResMut<Map>,
+    y1: i32,
+    y2: i32,
+    x: i32,
+    mut query: Query<&mut Tile>,
+) {
+    for y in min(y1, y2)..=max(y1, y2) {
+        let idx = get_tile_idx((x as usize, y as usize));
+        if idx > 0 && idx < 80 * 50 {
+            query.get_mut(map.tiles[idx]).unwrap().tiletype = TileType::Floor;
         }
     }
 }
