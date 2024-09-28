61aff4855ee55cbb2faeed0f7d50fab5e67c4660
biskwikman <djosephhenri@gmail.com>
2024-09-29 00:00:27 +0900

    raycasting implemented

--- src/visibility.rs
+++ src/visibility.rs
@@ -4,6 +4,8 @@ use crate::events::*;
 use crate::get_tile_idx;
 use crate::resources::*;
 use bevy::prelude::*;
+use bevy_rapier2d::math::Real;
+use std::iter::zip;
 pub struct VisibilityPlugin;
 use bevy_rapier2d::prelude::*;
 
@@ -36,7 +38,6 @@ fn get_viewshed(
 
     let player_tile_ent = map.tiles[get_tile_idx((player_pos.x, player_pos.y))];
 
-    // FOV direction crawl
     for (mut tile, _pos, _trans) in set.p0().iter_mut() {
         match tile.visibletype {
             VisibleType::Visible => {
@@ -57,7 +58,7 @@ fn get_viewshed(
         let x = radius * radian.cos();
         let y = radius * radian.sin();
 
-        let seen_tile = cast_ray(
+        let visible_tiles = cast_ray(
             &rapier_context,
             player_transform_x,
             player_transform_y,
@@ -67,11 +68,8 @@ fn get_viewshed(
             set.p1(),
         );
 
-        match seen_tile {
-            Some(..) => {
-                set.p1().get_mut(seen_tile.unwrap()).unwrap().0.visibletype = VisibleType::Visible;
-            }
-            None => {}
+        for tile in visible_tiles {
+            set.p1().get_mut(tile).unwrap().0.visibletype = VisibleType::Visible;
         }
     }
 }
@@ -83,8 +81,8 @@ fn cast_ray(
     tile_x: f32,
     tile_y: f32,
     player_tile_ent: Entity,
-    custom_query: Query<(&mut Tile, &Position)>,
-) -> Option<Entity> {
+    mut custom_query: Query<(&mut Tile, &Position)>,
+) -> Vec<Entity> {
     let ray_origin = Vec2::new(player_x, player_y);
     let ray_dir = Vec2::new(tile_x, tile_y);
     let max_toi = 1.0;
@@ -95,15 +93,68 @@ fn cast_ray(
             .get(handle)
             .is_ok_and(|tile| tile.0.tiletype == TileType::Wall)
     };
+    // let predicate2 = |handle| {
+    //     custom_query2
+    //         .get(handle)
+    //         .is_ok_and(|tile| tile.0.tiletype == TileType::Floor)
+    // };
     let filter = QueryFilter::exclude_rigid_body(n, player_tile_ent).predicate(&predicate);
+    let filter2 = QueryFilter::exclude_rigid_body(n, player_tile_ent);
 
-    if let Some((entity, _toi)) =
+    let mut intersected_tiles: Vec<Entity> = Vec::new();
+    let mut tile_intersections: Vec<RayIntersection> = Vec::new();
+
+    rapier_context.intersections_with_ray(
+        ray_origin,
+        ray_dir,
+        max_toi,
+        solid,
+        filter2,
+        |entity, intersection| {
+            intersected_tiles.push(entity);
+            tile_intersections.push(intersection);
+            true
+        },
+    );
+
+    let mut visible_tiles: Vec<Entity> = Vec::new();
+    let nearest_wall: Option<(Entity, f32)>;
+    // cast for nearest wall
+    if let Some((entity, toi)) =
         rapier_context.cast_ray(ray_origin, ray_dir, max_toi, solid, filter)
     {
-        Some(entity)
+        nearest_wall = Some((entity, toi));
+        // add nearest wall to visible tiles
+        if let Some(wall) = nearest_wall {
+            visible_tiles.push(wall.0);
+        }
+
+        for (ent, intersect) in zip(intersected_tiles.clone(), tile_intersections) {
+            if custom_query.get(ent).unwrap().0.tiletype != TileType::Wall
+                && intersect.time_of_impact < nearest_wall.unwrap().1
+            {
+                visible_tiles.push(ent);
+            }
+        }
     } else {
-        None
+        visible_tiles.append(&mut intersected_tiles);
     }
+
+    visible_tiles
+
+    // let mut lowest_toi: Real = Real::MAX;
+    // let mut closest_wall: Option<Entity> = None;
+    // for (ent, intersect) in zip(intersected_tiles.clone(), tile_intersections.clone()) {
+    //     if custom_query.get(ent).unwrap().0.tiletype == TileType::Wall
+    //         && intersect.time_of_impact < lowest_toi
+    //     {
+    //         closest_wall = Some(ent);
+    //         lowest_toi = intersect.time_of_impact;
+    //     }
+    //     if let Some(ent) = closest_wall {
+    //         visible_tiles.push(ent);
+    //     }
+    // }
 }
 
 fn apply_view(
